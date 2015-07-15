5dad1d482d7ccf14973d904f56b3c44c20eceb5c
Eric Katz <ericpkatz@gmail.com>
2015-07-15 23:57:04 +0000

    dynamic tabs

--- server.js
+++ server.js
@@ -6,19 +6,41 @@ app.set("view engine", "jade");
 
 app.use(express.static(__dirname + "/public"));
 
+app.use(function(req, res, next){
+    res.locals.tabs = [
+        {
+            title: "Home",
+            path: "/"
+        },
+        {
+           title: "People",
+           path: "/people"
+        },
+        {
+            title: "Things",
+            path: "/things"
+        }
+    ];
+    next(); 
+});
+
+
 app.get("/", function(req, res){
    res.render("index", {
-       title: "Home"
+       title: "Home",
+       activePath: "/"
    });
 });
 app.get("/people", function(req, res){
    res.render("people", {
-       title: "People"
+       title: "People",
+       activePath: "/people"
    });
 });
 app.get("/things", function(req, res){
    res.render("things", {
-       title: "Things"
+       title: "Things",
+       activePath: "/things"
    });
 });
 
