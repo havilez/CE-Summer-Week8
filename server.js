542cea2550780e325da6ea632024c701d9126224
Eric Katz <ericpkatz@gmail.com>
2015-07-15 23:31:40 +0000

    use a layout

--- server.js
+++ server.js
@@ -7,13 +7,19 @@ app.set("view engine", "jade");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
-   res.render("index");
+   res.render("index", {
+       title: "Home"
+   });
 });
 app.get("/people", function(req, res){
-   res.render("people");
+   res.render("people", {
+       title: "People"
+   });
 });
 app.get("/things", function(req, res){
-   res.render("things");
+   res.render("things", {
+       title: "Things"
+   });
 });
 
 app.listen(process.env.PORT);
